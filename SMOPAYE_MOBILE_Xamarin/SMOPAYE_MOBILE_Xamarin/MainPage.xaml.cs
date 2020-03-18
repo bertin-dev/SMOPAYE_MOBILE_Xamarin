ead73f7a23243fd309bee5d45a49ea42e3efc3c7
bertin <bertin.dev@outlook.fr>
2020-03-18 20:14:10 +0100

    Screen_Splash: Mise en place du screen splash au démarrage de l'application

--- SMOPAYE_MOBILE_Xamarin/SMOPAYE_MOBILE_Xamarin/MainPage.xaml.cs
+++ SMOPAYE_MOBILE_Xamarin/SMOPAYE_MOBILE_Xamarin/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SMOPAYE_MOBILE_Xamarin.Views;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -13,9 +14,40 @@ namespace SMOPAYE_MOBILE_Xamarin
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        private Image splashImage;
+
         public MainPage()
         {
-            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+            var sub = new AbsoluteLayout();
+            splashImage = new Image
+            {
+                //Source = "C:\\Users\\Bertin-dev\\Source\\Repos\\TestScreenSplash\\TestScreenSplash\\TestScreenSplash.Android\\Resources\\drawable\\logo_screen_splash.png",
+                Source = "logo_screen_splash.png",
+                WidthRequest = 100,
+                HeightRequest = 100
+            };
+            AbsoluteLayout.SetLayoutFlags(splashImage, AbsoluteLayoutFlags.PositionProportional);
+            AbsoluteLayout.SetLayoutBounds(splashImage, new Rectangle(0.5, 0.5, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
+            sub.Children.Add(splashImage);
+            //this.BackgroundColor = Color.FromHex("#429de3");
+            this.BackgroundImageSource = "bg_screen_splash.png";
+            this.Content = sub;
+
         }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await splashImage.ScaleTo(1, 2000);
+            await splashImage.ScaleTo(0.9, 1500, Easing.Linear);
+            await splashImage.ScaleTo(150, 1200, Easing.Linear);
+            Application.Current.MainPage = new NavigationPage(new LoginPage()); //after loading MainPage it gets Navigated
+        }
+
+
+
+
+
     }
 }
